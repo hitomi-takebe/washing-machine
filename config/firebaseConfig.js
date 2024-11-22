70ed1aef1a02edf99d510fc62b1c8babff8ebd47
hitomi-takebe <takebe.hitomi@gmail.com>
2024-11-23 00:16:12 +0900

    おためし４

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,11 +1,11 @@
-export const firebaseConfig = {
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    "AIzaSyAzgo4GGX8YmFoFDoFO1oa8rSEltSJWs0I",
-    authDomain: "dev28-27322.firebaseapp.com",
-    databaseURL: "https://dev28-27322-default-rtdb.firebaseio.com",
-    projectId: "dev28-27322",
-    storageBucket: "dev28-27322.firebasestorage.app",
-    messagingSenderId: "47494104071",
-    appId: "1:47494104071:web:33edcdd84cbbf9963fd6a2",
-    measurementId: "G-NTE1SBFX7R"
-};
+// export const firebaseConfig = {
+//     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//     "AIzaSyAzgo4GGX8YmFoFDoFO1oa8rSEltSJWs0I",
+//     authDomain: "dev28-27322.firebaseapp.com",
+//     databaseURL: "https://dev28-27322-default-rtdb.firebaseio.com",
+//     projectId: "dev28-27322",
+//     storageBucket: "dev28-27322.firebasestorage.app",
+//     messagingSenderId: "47494104071",
+//     appId: "1:47494104071:web:33edcdd84cbbf9963fd6a2",
+//     measurementId: "G-NTE1SBFX7R"
+// };
