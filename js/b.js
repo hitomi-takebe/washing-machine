1e02528cf209ad427fd068c403072d6e8272d5e7
hitomi-takebe <takebe.hitomi@gmail.com>
2024-11-26 19:33:31 +0900

    JSのファイル修正2

--- js/b.js
+++ js/b.js
@@ -13,14 +13,7 @@ import {
 // Firebaseの設定情報（Firebaseプロジェクトから取得）
 const firebaseConfig = {
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    apiKey: "AIzaSyAzgo4GGX8YmFoFDoFO1oa8rSEltSJWs0I",
-    authDomain: "dev28-27322.firebaseapp.com",
-    databaseURL: "https://dev28-27322-default-rtdb.firebaseio.com",
-    projectId: "dev28-27322",
-    storageBucket: "dev28-27322.firebasestorage.app",
-    messagingSenderId: "47494104071",
-    appId: "1:47494104071:web:33edcdd84cbbf9963fd6a2",
-    measurementId: "G-NTE1SBFX7R"
+
 };
 
 // Firebase初期化
